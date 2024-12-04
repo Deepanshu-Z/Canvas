48cccd3a812fe29ade7a74f18647e566d3bb941e
Deepanshu <deepanshupokhriyal07@gmail.com>
2024-12-04 08:33:50 +0530

    HAVE TO ADD THE PAINTING BEHAVIOUR THROUGH JS

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let i = 0
-while (i < (16*16)) {
+const h = 16;
+while (i < (h*h)) {
+    const rc = 660/h; 
     const divs = document.createElement('div');
-    divs.classList.add('grid');
+    divs.classList.add('grids');
+    divs.style.cssText = `height: ${rc}px; width :${rc}px`;
     const container = document.querySelector('.container');
     container.appendChild(divs);
     i++;
 }
+const grid = document.querySelectorAll('.grids');
+grid.forEach(function(e) {
+    e.addEventListener('', function() {
+        e.style.backgroundColor = "black"; // Directly apply styles to the clicked element
+    });
+});
