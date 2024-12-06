d064b2d82c2214a8986494570ee8225fc4918e85
Deepanshu <deepanshupokhriyal07@gmail.com>
2024-12-06 19:59:34 +0530

    ADDED CHANGE CANVAS AND ERASER FUNCTION TO THE CANVAS

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-let i = 0
-const h = 16;
-while (i < (h*h)) {
-    const rc = 660/h; 
-    const divs = document.createElement('div');
-    divs.classList.add('grids');
+let h = 16;
+function createCanvas(h) {
+  const container = document.querySelector(".container");
+  container.innerHTML = ""; 
+  let i = 0;
+  while (i < h * h) {
+    let rc = 660 / h;
+    const divs = document.createElement("div");
+    divs.classList.add("grids");
     divs.style.cssText = `height: ${rc}px; width :${rc}px`;
-    const container = document.querySelector('.container');
     container.appendChild(divs);
     i++;
-}
-const grid = document.querySelectorAll('.grids');
-grid.forEach(function(e) {
-    e.addEventListener('', function() {
-        e.style.backgroundColor = "black"; // Directly apply styles to the clicked element
+  }
+  const grid = document.querySelectorAll(".grids");
+  grid.forEach(function (e) {
+    e.addEventListener("mouseover", function () {
+      e.classList.remove("white");
+      e.style.backgroundColor = "black"; 
     });
-});
+  });
+}
+createCanvas(h);
+
+function erase() {
+  const grids = document.querySelectorAll(".grids");
+  grids.forEach(function (grid) {
+    grid.classList.add("white"); // Add the class without the dot prefix
+  });
+}
+
+function changeSize() {
+  let h = prompt("Enter the size of the display from 1 to 64: ");
+
+  h = Number(h);
+  if (h >= 1 && h <= 64) {
+    createCanvas(h);
+  } else {
+    alert("Please enter a valid number between 1 and 64.");
+    changeSize();
+  }
+}
